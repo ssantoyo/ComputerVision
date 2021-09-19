07430d8018c800a94505d18ba509a99969b9289c
Sergio Santoyo <santoyosergio14@gmail.com>
2021-09-18 18:28:40 -0700

    Stores changes as new image

--- tutorial1.py
+++ tutorial1.py
@@ -1,9 +1,12 @@
 import cv2
 
+
 img = cv2.imread('FlowerBoy.jpeg', 0)
 #resize dimensions of image
 img = cv2.resize(img, (0,0), fx=2, fy=2)
-img = cv2.rotate(img, cv2.cv2.ROTATE_90_CLOCKWISE)
+img = cv2.rotate(img, cv2.cv2.ROTATE_90_COUNTERCLOCKWISE)
+
+cv2.imwrite('new_img.jpeg', img)
 
 
 cv2.imshow('Image', img)
